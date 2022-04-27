87039ba23c15dc7c62875991e16f526e67ab09fc
Adithya Vijay K <adithyavj@gmail.com>
2022-04-27 19:20:14 +0530

    Added chatHub
    A class that acts as a server and sends message to the clients

---
+++ BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR; // SignalR is a wrapper around WebSockets
+
+namespace BlazorServer.Hubs;
+
+// This class acts as the server
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("RecieveMessage", user, message);
+    }
+}
